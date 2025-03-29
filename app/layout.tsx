df805f1415550c7d8900928cfd2bfa8a4f811956
Marcos Stevanini <devmarcosff@gmail.com>
2025-03-29 20:05:42 -0300

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { getMessages } from "next-intl/server";
 import { ThemeProvider } from "@/components/theme-provider";
 import { LoadingBar } from "@/components/loading-bar/loadgin-bar";
 import { Toaster } from "@/components/ui/sonner";
+import { Analytics } from "@vercel/analytics/react"
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -30,6 +31,7 @@ async function RootLayout({ children }: LayoutProps) {
   return (
     <html lang='pt-br'>
       <body className={poppins.className}>
+        <Analytics />
         <Suspense fallback={null}>
           <LoadingBar />
         </Suspense>
